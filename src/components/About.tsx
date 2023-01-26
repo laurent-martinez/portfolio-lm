c7e624d4cf6b27a5550a457d33b8fb405b6cff7c
laurent-martinez <martilaurier@gmail.com>
2023-01-26 11:22:49 +0100

    add spinner loader for pictures loading

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,24 +1,26 @@
 import Image from 'next/image';
 import AboutPic from '../../public/aboutpic3.png';
+import humanoid from '../../public/humanoid2.png';
+import loader from '../../public/loader.svg';
 import React, { useState } from 'react'
 import {useTranslation} from "react-i18next";
 type Props = {}
 
 export default function About({}: Props) {
-  const [image,setImage] = useState('/humanoid2.png');
+  const [image,setImage] = useState(humanoid);
   const {t} = useTranslation('fr',{useSuspense: false});
   return (
     <div className='flex flex-col relative text-center md:text-left md:flex-row h-screen max-w-7xl mx-auto items-center justify-center sm:justify-evenly p-10px '>
         <h3 className='absolute top-14 sm:top-10 uppercase tracking-[10px] sm:tracking-[20px] text-gray-500 text-2xl'>{t('About.title')}</h3>
+        {humanoid ?  
         <Image
         src={image}
-        onMouseEnter={()=>setImage('/aboutpic3.png')}
-        onMouseLeave={()=>setImage('/humanoid2.png')}
-        width={500} 
-        height={500}
+        onMouseEnter={()=>setImage(AboutPic)}
+        onMouseLeave={()=>setImage(humanoid)}
         className='cursor-pointer duration-500 rounded-full mt-[4em] md:mb-0 md:mt-0 flex-shrink-0 object-cover w-56 h-56 md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
         alt='picture of me'
-        />
+        /> : <Image alt='loader spinner' src={loader} className='animate-spin fill-[#F7AB0a] duration-50'/>}
+ 
         <div className='space-y-10 px-0 md:px-10'>
             <h4 className='text-4xl font-semibold font-aqua bg-clip-text text-transparent bg-gradient-to-r from-[#F7AB0A] to-[#f96531] dark:from-[#F7AB0A] dark:to-[#f1f3f1]'>{t('About.subTitle')}</h4>
             <p className='text-base p-5 line-clamp-6 sm:line-clamp-none font-roboto font-medium'>{t('About.text')} 💻💡🎨</p>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import axios from 'axios';
 import Link from 'next/link';
+import loader from '../../public/loader.svg';
 import React, { useEffect, useState } from 'react'
 import datas from '../Datas/datas.json';
 import {useTranslation} from "react-i18next";
@@ -35,9 +36,9 @@ export default function Projects({}: Props) {
              <div className='relative overflow-x-scroll overflow-y-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 w-full flex snap-x snap-mandatory z-20'>
                 {datas ? datas.map((project,index) =>(
                     <div key={index} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 sm:mb-10 items-center justify-start lg:justify-center  sm:py-10 px-20 '>
-                        <Image src={project.img} alt={project.title} className='min-h-[200px] min-w-[300px] md:h-[360px] md:w-[640px] mx-0 hidden lg:flex mt-0 pt-0 shadow-xl dark:shadow-[#F7AB0A]/30 ' width={640} height={360}/>
-                        <Image src={project.imgMobile} alt='photo mobile' width={170} height={360} className='w-[85px] h-[180px] shadow-xl dark:shadow-[#F7AB0A]/30  sm:hidden mt-0 pt-0'/>
-                        <Image src={project.imgTablet} alt='photo tablet' width={387} height={408} className='hidden sm:flex lg:hidden w-[258px] h-[272px] mt-0 pt-0 shadow-xl dark:shadow-[#F7AB0A]/30'/>
+                        {project.img ? <Image src={project.img} alt={project.title} className='min-h-[200px] min-w-[300px] md:h-[360px] md:w-[640px] mx-0 hidden lg:flex mt-0 pt-0 shadow-xl dark:shadow-[#F7AB0A]/30 ' width={640} height={360}/> : <Image alt='loader spinner' src={loader} className='animate-spin fill-[#F7AB0a] duration-50'/>}
+                        {project.imgMobile ? <Image src={project.imgMobile} alt='photo mobile' width={170} height={360} className='w-[85px] h-[180px] shadow-xl dark:shadow-[#F7AB0A]/30  sm:hidden mt-0 pt-0'/> : <Image alt='loader spinner' src={loader} className='animate-spin fill-[#F7AB0a] duration-50'/>}
+                        {project.imgTablet ? <Image src={project.imgTablet} alt='photo tablet' width={387} height={408} className='hidden sm:flex lg:hidden w-[258px] h-[272px] mt-0 pt-0 shadow-xl dark:shadow-[#F7AB0A]/30'/> : <Image alt='loader spinner' src={loader} className='animate-spin fill-[#F7AB0a] duration-50'/>}
                         <div className=' px-0 md:px-10 max-w-6xl flex flex-col items-center'>
                             <Link className='font-semibold text-center mx-auto text-2xl whitespace-nowrap sm:text-4xl font-aqua hover:text-[#F7AB0A]' href={project.src} target='_blank'><span className='underline decoration-[#F7AB0A]/50'>{project.title}</span> </Link>
                             <Link href={project.repo} className='italic font-thin text-gray-700 dark:text-gray-300 dark:hover:text-[#F7AB0A] text-lg hover:text-[#F7AB0A] ' target='_blank'>CODE</Link>
